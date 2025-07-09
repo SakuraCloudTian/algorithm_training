a90afc49b872bd98af3c4ed6768296dc72dded30
Yin Kun <94735467+SakuraCloudTian@users.noreply.github.com>
2025-07-09 15:43:34 +0900

    Time: 2726 ms (42.43%), Space: 18 MB (64.62%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp=[n]*(n+1)
+        dp[0]=0
+        for i in range(1,n+1):
+            j=1
+            while j*j<=i:
+                dp[i]=min(dp[i-j*j]+1,dp[i])
+                j+=1
+                
+        return dp[n]
+        
